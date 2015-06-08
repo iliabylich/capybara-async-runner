45367b25d79b87292991c0e38edc851b84e918b1
Ilya Bylich <ibylich@gmail.com>
2015-06-08 19:40:24 +0300

    Initial commit.

---
+++ lib/capybara/async_runner.rb
@@ -0,0 +1,7 @@
+require "capybara/async_runner/version"
+
+module Capybara
+  module AsyncRunner
+    # Your code goes here...
+  end
+end
---
+++ lib/capybara/async_runner/version.rb
@@ -0,0 +1,5 @@
+module Capybara
+  module AsyncRunner
+    VERSION = "0.1.0"
+  end
+end
